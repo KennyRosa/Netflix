4105de6af668c152845a95667fe3b8e99929927c
Kenny Rosa <112502866+KennyRosa@users.noreply.github.com>
2023-01-27 21:09:14 -0500

    Update Landing.jsx

--- netflix/src/components/Landing.jsx
+++ netflix/src/components/Landing.jsx
@@ -1,22 +1,21 @@
 import React from "react";
-// import AnnouncementHeader from "../components/AnnouncementHeader";
-// import NavigateBar from "../components/NavigateBar";
-// import EnjoyTV from "../components/EnjoyTV";
-// import CreateChildrenProfile from "../components/CreateChildrenProfile";
-// import DownloadShows from "../components/DownloadShows";
-// import FAQ from "../components/FAQ";
-import MovieList from "./MovieList";
+import AnnouncementHeader from "../components/AnnouncementHeader";
+import NavigateBar from "../components/NavigateBar";
+import EnjoyTV from "../components/EnjoyTV";
+import CreateChildrenProfile from "../components/CreateChildrenProfile";
+import DownloadShows from "../components/DownloadShows";
+import FAQ from "../components/FAQ";
+
 
 function Landing() {
   return (
     <React.Fragment>
-      {/* <AnnouncementHeader />
+      <AnnouncementHeader />
       <NavigateBar />
       <EnjoyTV />
       <DownloadShows />
       <CreateChildrenProfile />
       <FAQ /> */}
-      <MovieList />
     </React.Fragment>
   );
 }
