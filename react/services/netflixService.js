585d3933da5dd196daeaefbf216bcd9e71c46f98
Kenny Rosa <112502866+KennyRosa@users.noreply.github.com>
2023-02-02 21:13:16 -0500

    Create netflixService.js

---
+++ react/services/netflixService.js
@@ -0,0 +1,33 @@
+import axios from "axios"
+
+const endpoint = `https://api.remotebootcamp.dev/api/users/login`
+
+const userLogIn = () => {
+
+  const config = {
+    method: "POST",
+    url: endpoint,
+    withCredentials: true,
+    crossdomain: true,
+    headers: { "Content-Type": "application/json" }
+  };
+  
+  return axios(config);
+};
+
+
+const getMovies = () => {
+  const config = {
+  method: 'GET',
+  url: 'https://imdb-top-100-movies.p.rapidapi.com/',
+  headers: {
+    'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com'
+  }
+};
+
+return axios.request(config)
+}
+
+const netflixService = { userLogIn, getMovies }
+
+export default netflixService;
