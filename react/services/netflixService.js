0bcbb36dec6850e10248c0cad34623368aa67f9e
Kenny Rosa <112502866+KennyRosa@users.noreply.github.com>
2023-02-02 21:34:08 -0500

    Update netflixService.js

--- react/services/netflixService.js
+++ react/services/netflixService.js
@@ -2,10 +2,11 @@ import axios from "axios"
 
 const endpoint = `https://api.remotebootcamp.dev/api/users/login`
 
-const userLogIn = () => {
+const userLogIn = (payload) => {
 
   const config = {
     method: "POST",
+    data: payload,
     url: endpoint,
     withCredentials: true,
     crossdomain: true,
@@ -21,6 +22,7 @@ const getMovies = () => {
   method: 'GET',
   url: 'https://imdb-top-100-movies.p.rapidapi.com/',
   headers: {
+    'X-RapidAPI-Key': '219895c7c2msh60b15435f51b095p13232ajsn053ddcf635f4',
     'X-RapidAPI-Host': 'imdb-top-100-movies.p.rapidapi.com'
   }
 };
